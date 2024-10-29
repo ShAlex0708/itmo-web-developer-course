433608d740a782509e217449d1bfdb28bbb8ca8e
Aleksandr Shelkovin <shelkovin.sashka@gmail.com>
2024-10-29 12:13:04 +0300

    this

--- clickThisForm/scripts/sripts.js
+++ clickThisForm/scripts/sripts.js
@@ -1,12 +1,37 @@
-let selectedColor = 'red'; // По умолчанию выбран красный
-  
+let selectedColor = 'red';
+const colorBoxes = document.querySelectorAll('.color-option');
+colorBoxes.forEach(function (colorBox) {
+  colorBox.addEventListener('click', function () {
+    selectedColor = getComputedStyle(this).background;
+  });
+});
+const coloringBoxes = document.querySelectorAll('.EmptyBox');
+coloringBoxes.forEach(function (emptyBox) {
+  emptyBox.addEventListener('click', function () {
+    if (selectedColor) {
+      this.style.background = selectedColor;
+    }
+  });
+});
 
-const container = document.querySelector(".container")
-container.addEventListener("click", function(ev){
-  const dataColor = ev.target.getAttribute("data-color")
-  if(dataColor){
-    selectedColor = dataColor
-  } else if (ev.target.classList.contains("EmptyBox")) {
-    ev.target.style.background = selectedColor
-  }
-})
+// const resetButton = document.querySelector('.button');
+// let resetBoxes = getComputedStyle.background
+// resetButton.addEventListener('click', function () {
+//   resetBoxes.forEach(resetBox => {
+//     resetBox.style.backgroundColor = 'grey'
+//   });
+//   })
+
+
+
+
+
+// const container = document.querySelector(".container")
+// container.addEventListener("click", function(ev){
+//   const dataColor = ev.target.getAttribute("data-color")
+//   if(dataColor){
+//     selectedColor = dataColor
+//   } else if (ev.target.classList.contains("EmptyBox")) {
+//     ev.target.style.background = selectedColor
+//   }
+// })
